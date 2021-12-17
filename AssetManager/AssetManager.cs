46bcb0543ad0d3f410fc0de902c1df7cf3820f2a
Bram Deraeve <bram.deraeve@telenet.be>
2021-12-17 23:06:49 +0100

    loading textures by name

--- AssetManager/AssetManager.cs
+++ AssetManager/AssetManager.cs
@@ -9,16 +9,69 @@ namespace RaylibExt
     {
         // a huge dictionary that stores all of your assets
         private Dictionary<string, Asset> loadedAssets;
-        public AssetManager(string path)
+
+        private Texture2D _placeHolder;
+
+        public AssetManager(string path, string placeHolderTexturePath)
         {
+            _placeHolder = Raylib.LoadTexture(placeHolderTexturePath);
+
+            // add a trailing / to the end of the path if there isn't one already
+            if (!path.EndsWith('/'))
+                path += '/';
+            
             Raylib.TraceLog(TraceLogLevel.LOG_INFO, $"Loading assets at {path}");
+            int chopLength = path.Length;            
             loadedAssets = new Dictionary<string, Asset>();
+           
             // index asset files
             List<string> files = Utils.CrawlDirectory(path);
             foreach (var file in files)
             {
                 Raylib.TraceLog(TraceLogLevel.LOG_DEBUG, $"Loading {file}...");
+                
+                // generate name for the asset
+                // chop of the "assets" portion from the path, change slashes in path to underscores, remove extension
+                string name = file.Remove(0,chopLength).Replace('/', '_').Replace('\\','_');
+                name = name.Substring(0,name.LastIndexOf('.'));
+                if (loadedAssets.ContainsKey(name))
+				{
+                    Raylib.TraceLog(TraceLogLevel.LOG_WARNING,$"Asset named {name} already exists!");
+				    continue;
+                } 
+                // determine file type from path
+                Asset asset = null;
+                if (file.EndsWith(".png") || file.EndsWith(".jpg"))
+				{
+                    asset = new TextureAsset(file);
+				} //else if (file.EndsWith(".wav") || file.EndsWith(".ogg") || file.EndsWith(".flac") || file.EndsWith(".mp3")){
+                   // not implemented 
+                //}
+				else
+				{
+                    Raylib.TraceLog(TraceLogLevel.LOG_WARNING,$"Unknown file format of file {file}, ignoring...");
+				    continue;
+                }
+                loadedAssets.Add(name,asset); // TODO multiple files handling
+                Raylib.TraceLog(TraceLogLevel.LOG_DEBUG, $"Assigned name {name} to asset {file}");
             }
+
+
+        }
+        
+        public Texture2D GetTexture(string name)
+		{
+            if (loadedAssets.ContainsKey(name))
+			{
+                Asset item = loadedAssets[name];
+                if (item.GetType() == typeof(TextureAsset))
+				{
+                    return ((TextureAsset)item).Texture;
+				}
+                return _placeHolder; // TODO replace with default texture
+            }
+            Raylib.TraceLog(TraceLogLevel.LOG_WARNING,$"Texture {name} doesn't exist!");
+		    return _placeHolder;
         }
 
         public void Dispose()
--- AssetManager/AssetTypes.cs
+++ AssetManager/AssetTypes.cs
@@ -7,31 +7,28 @@ namespace RaylibExt
 {
     public abstract class Asset : IDisposable
     {
-        protected string sourcePath;
+        protected string _sourcePath;
+        
         public Asset(string sourcePath)
         {
-            this.sourcePath = sourcePath;
+            _sourcePath = sourcePath;
         }
-        public abstract string GenerateName(string path);
         public abstract void Dispose();
     }
     public class TextureAsset : Asset 
     {
-        private Texture2D texture;
+        private Texture2D _texture;
+        
+        public Texture2D Texture => _texture;
+        
         public TextureAsset(string path) : base(path)
         {
-            texture = Raylib.LoadTexture(path);
+            _texture = Raylib.LoadTexture(path);
         }
-        public override string GenerateName(string path)
-        {
-            // change slashes to underscores
-            string name = path.Replace('/', '_');
-            Raylib.TraceLog(TraceLogLevel.LOG_DEBUG, $"Assigning name {name} to asset {path}");
-            return name;
-        }
+        
         public override void Dispose()
         {
-            Raylib.UnloadTexture(texture);
+            Raylib.UnloadTexture(_texture);
         }
     }
 }
